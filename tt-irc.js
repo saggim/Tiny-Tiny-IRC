2187789aacc7f19d887683fb846d18a7fc4177f7
Andrew Dolgov <fox@bah.org.ru>
2010-01-28 16:16:28 +0300

    piggie patch

--- tt-irc.js
+++ tt-irc.js
@@ -691,6 +691,9 @@ function format_message(row_class, param, connection_id) {
 					userhosts[param.sender][1] + " <" + userhosts[param.sender][3] + ">";
 			}					
 
+			param.message = param.message.replace("(oo)",
+					"<img src='images/piggie_icon.png' alt='(oo)'>");
+
 			tmp = "<li class=\""+row_class+"\"><span class='timestamp'>" + 
 				param.ts + "</span><span title=\""+nick_ext_info+"\" " +
 				"class='sender' "+color+">&lt;" +
