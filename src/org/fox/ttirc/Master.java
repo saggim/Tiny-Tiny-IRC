aa2caeb0404834839dcb73b524e3098022991ddd
Andrew Dolgov <fox@bah.org.ru>
2010-02-05 18:11:47 +0300

    add java-related stuff

---
+++ src/org/fox/ttirc/Master.java
@@ -0,0 +1,31 @@
+package org.fox.ttirc;
+
+import java.sql.*;
+import java.util.*;
+
+public class Master {
+
+	protected Connection conn;
+		
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Master m = new Master();
+		
+		m.Run();
+				
+	}
+	
+	public Master() {
+			
+	}
+	
+	public void Run() {
+		
+		
+	}
+
+}
